d250526783060bba5b9b79549c5ef457a845d303
maksmolchdmitr <62752481+maksmolchdmitr@users.noreply.github.com>
2022-10-25 02:19:58 +0300

    add uploading documents and photos in attachments and create more flexible interface for sending message

--- src/main/java/maks/molch/dmitr/firstrightvkbot/vk/VkBotConfig.java
+++ src/main/java/maks/molch/dmitr/firstrightvkbot/vk/VkBotConfig.java
@@ -6,42 +6,52 @@ import com.vk.api.sdk.client.actors.GroupActor;
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
-import com.vk.api.sdk.objects.messages.Message;
+import com.vk.api.sdk.queries.messages.MessagesSendQuery;
+import com.vk.api.sdk.queries.upload.UploadDocQuery;
+import com.vk.api.sdk.queries.upload.UploadPhotoMessageQuery;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 @Component
 @PropertySource("application.properties")
 public class VkBotConfig {
-    private int groupId;
-    private String accessToken;
     @Value("${vk.bot.response}")
     private String response;
-
-
-    public int getGroupId() {
-        return groupId;
-    }
-
-    public String getResponse() {
-        return response;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public VkApiClient getVk() {
-        return vk;
-    }
-
+    private int groupId;
+    private String accessToken;
     private TransportClient transportClient;
     private VkApiClient vk;
     private GroupActor actor;
+    private Random random = new Random();
+    private static final int GROUP_PEER_ID = 2000000000;
+    private static final Set<String> fileExtensionsDenied = new HashSet<>(Arrays.asList(
+            "exe",
+            "mp3"
+    ));
+
+    public MessagesSendQuery getMessagesSendQuery(int peerId){
+        if(peerId>=GROUP_PEER_ID){
+            return this.vk.messages().send(this.actor).chatId(peerId-GROUP_PEER_ID).randomId(random.nextInt(10000));
+        }else{
+            return this.vk.messages().send(this.actor).userId(peerId).randomId(random.nextInt(10000));
+        }
+    }
+    public int getGroupId() {
+        return groupId;
+    }
+    public String getResponse() {
+        return response;
+    }
 
     public VkBotConfig(@Value("${vk.bot.groupId}") int groupId, @Value("${vk.bot.accessToken}") String accessToken){
         this.groupId = groupId;
@@ -50,24 +60,41 @@ public class VkBotConfig {
         this.vk = new VkApiClient(this.transportClient);
         this.actor = new GroupActor(this.groupId, this.accessToken);
     }
-
-    public void sendMessage(Message message){
-        Integer peerId = message.getPeerId();
-        Random random = new Random();
-        if(peerId>=2000000000){
-            try {
-                Integer ts = vk.messages().getLongPollServer(actor).execute().getTs();
-                vk.messages().send(actor).message("Haha:)").chatId(peerId-2000000000).randomId(random.nextInt(10000)).execute();
-            } catch (ApiException | ClientException e) {
-                throw new RuntimeException(e);
+    public String getUploadDocAttachId(File file, int peerId) throws IOException, ClientException, ApiException, JSONException {
+        String[] fileParts = file.getName().split("\\.");
+        String fileExtension = fileParts[fileParts.length-1];
+        if(fileExtensionsDenied.contains(fileExtension)){
+            File tempFile = new File(file.getAbsolutePath()+".delete.me");
+            if(!file.renameTo(tempFile)){
+                throw new FileNotFoundException("Not successful renaming file...");
             }
-        }else {
-            try {
-                Integer ts = vk.messages().getLongPollServer(actor).execute().getTs();
-                vk.messages().send(actor).message("Haha:)").userId(peerId).randomId(random.nextInt(10000)).execute();
-            } catch (ApiException | ClientException e) {
-                throw new RuntimeException(e);
-            }
+            file = tempFile;
         }
+        UploadDocQuery uploadDocQuery = vk.upload().doc(vk.docs().getMessagesUploadServer(actor).peerId(peerId).execute().
+                getUploadUrl().toString(), file);
+        JSONObject jsonObject = new JSONObject(uploadDocQuery.executeAsString());
+        try{
+            JSONObject json = new JSONObject(this.vk.docs().save(this.actor, jsonObject.getString("file")).executeAsString());
+            JSONObject jsonObj = json.getJSONObject("response").getJSONObject("doc");
+            return "doc"+jsonObj.getLong("owner_id")+"_"+jsonObj.getLong("id");
+        }catch (JSONException e){
+            System.out.println("Add extension '"+fileExtension+"' in DENIED EXTENSIONS!!!");
+            return "";
+        }
+    }
+    public String getUploadPhotoAttachId(File file, int peerId) throws IOException, ClientException, ApiException, JSONException {
+        URI url = vk.photos().getMessagesUploadServer(actor).peerId(peerId).execute().getUploadUrl();
+        UploadPhotoMessageQuery uploadPhotoMessageQuery = vk.upload().photoMessage(url.toString(), file);
+        JSONObject photoJsonObject = new JSONObject(uploadPhotoMessageQuery.executeAsString());
+        JSONObject responseJsonObject = new JSONObject(vk.photos().
+                saveMessagesPhoto(actor, photoJsonObject.getString("photo")).
+                server(photoJsonObject.getInt("server")).
+                hash(photoJsonObject.getString("hash"))
+                .executeAsString());
+        JSONObject content = responseJsonObject.
+                getJSONArray("response").getJSONObject(0);
+        int ownerId = content.getInt("owner_id");
+        int id = content.getInt("id");
+        return "photo"+ownerId+"_"+id;
     }
 }
--- src/main/java/maks/molch/dmitr/firstrightvkbot/vk/VkController.java
+++ src/main/java/maks/molch/dmitr/firstrightvkbot/vk/VkController.java
@@ -1,17 +1,22 @@
 package maks.molch.dmitr.firstrightvkbot.vk;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.objects.callback.Base;
 import com.vk.api.sdk.objects.messages.Message;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Random;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 public class VkController {
@@ -19,9 +24,16 @@ public class VkController {
     @Autowired
     VkBotConfig vkBotConfig;
 
+    private File uploadedFilesFolder = new File("src/main/resources/uploadedFiles");
+    private List<String> photos = Arrays.asList(
+            "pain.jpg",
+            "Russia.png"
+    );
+
+
     @PostMapping()
     public String requestHandle(@RequestBody String str){
-        System.out.println("JSON: "+str);
+//        System.out.println("JSON: "+str);
         JSONObject jsonObj = new JSONObject(str);
         Gson gson = new Gson();
         Base base = gson.fromJson(str, Base.class);
@@ -39,16 +51,77 @@ public class VkController {
                 return "ok";
             case MESSAGE_REPLY:
                 return "ok";
+            case MESSAGE_TYPING_STATE:
+                int peerId = jsonObj.getJSONObject("object").getInt("from_id");
+                typingUpdate(peerId);
+                return "ok";
             default:
                 System.out.println("Unknown type: " + base.getType());
                 return "ok";
         }
     }
 
+    private void typingUpdate(int peerId){
+        try {
+            vkBotConfig.getMessagesSendQuery(peerId).message("I'm all ears...").execute();
+        } catch (ApiException|ClientException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private void onUpdate(Message message){
-        System.out.println("Message:" + message.toString());
-        System.out.println("Text:" + message.getText());
-        vkBotConfig.sendMessage(message);
+//        System.out.println("Text:" + message.getText());
+        int peerId = message.getPeerId();
+        Matcher matcherDoc = Pattern.compile("/sendMeDocument").matcher(message.getText());
+        Matcher matcherPhoto = Pattern.compile("/sendMePhoto").matcher(message.getText());
+        Matcher matcherHelp = Pattern.compile("/help").matcher(message.getText());
+        Random r = new Random();
+        try {
+            if (matcherDoc.find()) {
+                vkBotConfig.getMessagesSendQuery(peerId).message("I'm sending doc").execute();
+                vkBotConfig.getMessagesSendQuery(peerId).message(".").execute();
+                vkBotConfig.getMessagesSendQuery(peerId).message("..").execute();
+                vkBotConfig.getMessagesSendQuery(peerId).message("...").execute();
+                vkBotConfig.getMessagesSendQuery(peerId).message("Your doc:").
+                        attachment(vkBotConfig.getUploadDocAttachId(
+                                Objects.requireNonNull(uploadedFilesFolder.listFiles())
+                                        [r.nextInt(1000)%uploadedFilesFolder.listFiles().length],
+                                        peerId)).execute();
+            } else if(matcherPhoto.find()) {
+                vkBotConfig.getMessagesSendQuery(peerId).message("I'm sending photo").execute();
+                vkBotConfig.getMessagesSendQuery(peerId).message(".").execute();
+                vkBotConfig.getMessagesSendQuery(peerId).message("..").execute();
+                vkBotConfig.getMessagesSendQuery(peerId).message("...").execute();
+                vkBotConfig.getMessagesSendQuery(peerId).message("Your photo:").attachment(
+                                vkBotConfig.getUploadPhotoAttachId(new File("src/main/resources/uploadedFiles/"+
+                                                photos.get(r.nextInt(1000)%photos.size())),
+                                        peerId)).execute();
+            } else if (matcherHelp.find()) {
+                vkBotConfig.getMessagesSendQuery(peerId).
+                        message("/help").execute();
+                vkBotConfig.getMessagesSendQuery(peerId).
+                        message("- List of all commands").execute();
+                vkBotConfig.getMessagesSendQuery(peerId).
+                        message("/sendMeDocument").execute();
+                vkBotConfig.getMessagesSendQuery(peerId).
+                        message("- sending document for you").execute();
+                vkBotConfig.getMessagesSendQuery(peerId).
+                        message("/sendMePhoto").execute();
+                vkBotConfig.getMessagesSendQuery(peerId).
+                        message(" - sending photo for you").execute();
+            }else{
+                vkBotConfig.getMessagesSendQuery(peerId).message("This command doesn't exist\n" +
+                        "You can send /help to help").execute();
+            }
+        }catch (IOException|ClientException|ApiException|JSONException e) {
+//            try {
+//                vkBotConfig.getMessagesSendQuery(peerId).
+//                        message("Sorry for happening error...").execute();
+//            } catch (ApiException|ClientException ex) {
+//                throw new RuntimeException(ex);
+//            }
+            throw new RuntimeException(e);
+        }
     }
 
 }
